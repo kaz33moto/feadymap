2082419a8f08d438c5e5c8127424777d1e9972cb
kaz33moto <91957437+kaz33moto@users.noreply.github.com>
2021-12-25 21:48:25 +0900

    Add files via upload

---
+++ startpage.js
@@ -0,0 +1,20 @@
+var target = document.getElementById("box1");
+target.href = "2021:12:24.html";
+
+blogtitle1.innerHTML = "ちょうど良すぎる！　Helloluluのシンプルなサコッシュ Desi ";
+blogimg1.innerHTML = '<img src="https://assets.st-note.com/production/uploads/images/68409963/rectangle_large_type_2_052ec14a9c5090f7531ec2b35553ee21.png?width=800" alt="">';
+date1.innerHTML = "2021/12/24";
+
+
+var target = document.getElementById("box2");
+target.href = "";
+
+blogtitle2.innerHTML = "";
+blogimg2.innerHTML = '<img src="" alt="">';
+
+
+var target = document.getElementById("box3");
+target.href = "";
+
+blogtitle3.innerHTML = "";
+blogimg3.innerHTML = '<img src="" alt="">';
