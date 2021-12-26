de299ad638ce5133c5c28b0bbbbb2706ba050ace
kaz33moto <91957437+kaz33moto@users.noreply.github.com>
2021-12-26 21:12:39 +0900

    Update startpage.js

--- startpage.js
+++ startpage.js
@@ -15,7 +15,8 @@ date1.innerHTML = "2021/12/24";
 
 blogtitle2.innerHTML = "コスパ最強　Huawei band 6 ";
 blogimg2.innerHTML = '<img src="https://assets.st-note.com/production/uploads/images/67607142/rectangle_large_type_2_1a52536c050d4c02f481efb0dffa064d.png?width=800" alt="">';
+date2.innerHTML = "2021/12/24";
 
 blogtitle3.innerHTML = "";
 blogimg3.innerHTML = '<img src="" alt="">';
-
+date3.innerHTML = "2021/12/24";
