218ee85032de4437bc604a66a8cd532ad736bcc8
kaz33moto <91957437+kaz33moto@users.noreply.github.com>
2022-01-23 14:14:40 +0900

    Add files via upload

---
+++ startpage.js
@@ -0,0 +1,22 @@
+var target = document.getElementById("box1");
+target.href = "2021:12:24.html";
+
+var target = document.getElementById("box2");
+target.href = "2021:12:12.html";
+
+var target = document.getElementById("box3");
+target.href = "";
+
+
+
+blogtitle1.innerHTML = "ちょうど良すぎる！　Helloluluのシンプルなサコッシュ Desi ";
+blogimg1.innerHTML = '<img src="https://assets.st-note.com/production/uploads/images/68409963/rectangle_large_type_2_052ec14a9c5090f7531ec2b35553ee21.png?width=800" alt="">';
+date1.innerHTML = "2021/12/24";
+
+blogtitle2.innerHTML = "コスパ最強　Huawei band 6 ";
+blogimg2.innerHTML = '<img src="https://assets.st-note.com/production/uploads/images/67607142/rectangle_large_type_2_1a52536c050d4c02f481efb0dffa064d.png?width=800" alt="">';
+date2.innerHTML = "2021/12/24";
+
+blogtitle3.innerHTML = "";
+blogimg3.innerHTML = '<img src="" alt="">';
+date3.innerHTML = "";
